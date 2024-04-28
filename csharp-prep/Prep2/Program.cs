d0b61a04ad440db89b6da017354d776ce42f5a4d
CalebAbdom <calebabdom@icloud.com>
2024-04-27 22:23:54 -0600

    prep activites 1-5 done

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What was your grade percentage? ");
+        string input = Console.ReadLine();
+        int grade = int.Parse(input);
+
+        string letter = "";
+
+
+        if (grade >= 90 && grade <= 100)
+        {
+            letter = "A";
+        }
+
+        else if (grade >= 80)
+        {
+            letter = "B";
+            
+        }
+
+        else if (grade>= 70)
+        {
+            letter = "C";
+        }
+
+        else if (grade >= 60)
+        {
+            letter  = "D";
+        }
+
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is an {letter}.");
+
+        if (grade >=70)
+        {
+            Console.WriteLine("You Passed!");
+        }
+
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
